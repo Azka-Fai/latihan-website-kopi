4724dfac04a6efd5e8c8cb97870b01f6bff4c5ae
Azka-Fai <ozza5lima@gmail.com>
2025-12-09 14:56:34 +0700

    inisialisasi projek

---
+++ js/newsletter.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const checkbox  = document.getElementById("langganan");
+    checkbox.addEventListener("change", () => {
+        if (checkbox.checked) {
+            alert("Terimakasih telah berlangganan!");
+        } else{
+            alert("Langganan di batalkan. ");
+        }
+    });
+});
